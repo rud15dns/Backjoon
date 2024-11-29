e858c9a8e94bcab711d35364f730831ae271b37a
rud15dns <rud15dns@naver.com>
2024-11-29 13:00:55 +0900

    0 0 0 0 인 경우만 빼면 됨

--- 9375.py
+++ 9375.py
@@ -1,4 +1,7 @@
 #이거 내 생각에는 k를 계속해서 반복문 돌리는? 반복문 안에 반복문 이런 식으로 풀어야 할 것 같다.
+#이라고 생각했지만, 사실 안 입는 경우도 포함시킨다음에, 0 0 0 0 0인 그런 경우만 하나 빼면 되기 때문에
+#풀 수 있었다.
+
 import sys
 n = int(input())
 
@@ -13,20 +16,14 @@ for _ in range(n):
         else:
             graph[key].append(value)
 
-    # print(graph)
-    tot = 0
     mul = 1
 
     
     for i in graph.keys():
         
-        mul *= len(graph[i])
-        tot += len(graph[i])
+        mul *= (len(graph[i]) + 1)
 
             
-    if len(graph) > 1:   
-        print(mul + tot)
-    else:
-        print(tot)
+    print(mul - 1)
     
     
